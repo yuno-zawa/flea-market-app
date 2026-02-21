e85d6ff6ad345d302ac4eae563a5e0b43aafe988
yunozawa <yuria12290224@gmail.com>
2026-02-21 23:49:32 +0900

    住所変更画面の作成

--- src/app/Http/Requests/AddressRequest.php
+++ src/app/Http/Requests/AddressRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CommentRequest extends FormRequest
+class AddressRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,14 +24,18 @@ class CommentRequest extends FormRequest
     public function rules()
     {
         return [
-            'content' => 'required|string|max:255',
+            'postal_code' => ['required', 'regex:/^\d{3}-\d{4}$/'],
+            'address' => ['required', 'string'],
+            'building' => ['nullable', 'string'],
         ];
     }
+
     public function messages()
     {
         return [
-            'content.required' => 'コメントを入力してください',
-            'content.max' => 'コメントは255文字以内で入力してください',
+            'postal_code.required' => '郵便番号を入力してください',
+            'postal_code.regex' => '郵便番号はハイフンを含む8文字で入力してください（例：123-4567）',
+            'address.required' => '住所を入力してください',
         ];
     }
 }
---
+++ src/resources/views/purchase/address.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('title', '配送先の変更')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+@endsection
+
+@section('content')
+<div class="login-container">
+    <h1>住所の変更</h1>
+    <form method="POST" action="{{ route('purchase.address.update', $item->id) }}">
+        @csrf
+        
+        <div class="form-group">
+            <label for="postal_code">郵便番号</label>
+            <input 
+                id="postal_code" 
+                type="text" 
+                name="postal_code" 
+                value="{{ old('postal_code', session('shipping_postal_code', $user->postal_code)) }}"
+                placeholder="123-4567"
+            >
+            @error('postal_code')
+                <p class="error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="address">住所</label>
+            <input 
+                id="address" 
+                type="text" 
+                name="address" 
+                value="{{ old('address', session('shipping_address', $user->address)) }}"
+                placeholder="東京都渋谷区千駄ヶ谷1-2-3"
+            >
+            @error('address')
+                <p class="error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="building">建物名</label>
+            <input 
+                id="building" 
+                type="text" 
+                name="building" 
+                value="{{ old('building', session('shipping_building', $user->building)) }}">
+            @error('building')
+                <p class="error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <button type="submit">更新する</button>
+    </form>
+</div>
+@endsection
